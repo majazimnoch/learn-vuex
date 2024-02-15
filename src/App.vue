e0805e7cef79ee500f862d9b50a6d20b2c32fb24
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-02-15 11:54:06 +0100

    kicking off

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
+<div class="home">
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="counter">
+    0
+  </div>
+  <div class="buttons">
+    <button>-</button>
+    <button>+</button>
+  </div>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  
   }
 }
 </script>
@@ -17,8 +25,6 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
