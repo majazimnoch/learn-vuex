a374e350fae2c9d113b11f98bc9b8c8cd4bd09d8
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-02-16 22:17:53 +0100

    added counter to state

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,53 @@
 <template>
-<div class="home">
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div class="counter">
-    {{  counter }}
+  <div class="home">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="counter">
+      {{  $store.state.counter }}
+    </div>
+    <div class="buttons">
+      <button @click="decreaseCounter">-</button>
+      <button @click="increaseCounter">+</button>
+    </div>
   </div>
-  <div class="buttons">
-    <button @click="decreaseCounter">-</button>
-    <button @click="increaseCounter">+</button>
-  </div>
-</div>
-
-</template>
-
-<script>
-
-export default {
-  name: 'App',
-  components: {
   
-  },
-  data(){
-    return {
-      counter: 0
-    }
-  }, 
-  methods: {
-    increaseCounter() {
-      this.counter++
+  </template>
+  
+  <script>
+
+import store from './store'; 
+  
+  export default {
+    name: 'App',
+    methods: {
+      decreaseCounter() {
+        this.counter--
+      },
+      increaseCounter() {
+        this.counter++
+      }
     },
-    decreaseCounter() {
-      this.counter--
-    }
+    store
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.home {
-  margin-bottom: 10px;
-}
-.counter {
-font-size: 80px;
-}
-
-.buttons button {
-  font-size: 40px;
-  width: 100px;
-  margin: 0 10px;
-}
-</style>
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  .home {
+    margin-bottom: 10px;
+  }
+  .counter {
+  font-size: 80px;
+  }
+  
+  .buttons button {
+    font-size: 40px;
+    width: 100px;
+    margin: 0 10px;
+  }
+  </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
     state: {
-        
+        counter: 0
     },
     mutations: {
 
     },
     actions: {
+
+    },
+    getters: {
 
     },
     modules: {
